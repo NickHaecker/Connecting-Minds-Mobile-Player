3e8c07574e6d82ca8ffde3995a48e50e12ff4897
SuzanJ <132355269+SuzanJ@users.noreply.github.com>
2023-12-04 08:18:37 +0100

    Changes

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -7,16 +7,29 @@
           <p>Score: {{ level }}</p>
           <p>Level: {{ score }}</p>
         </div>
-        <div class="game-board">
-          <!-- Hier kommt dein Spielbrettinhalt hin -->
-        </div>
-        <section class="game-controls">
-          <button @click= "Select" class="control-button">Select</button> 
+       <section class="game-controls">
+          <button @click="()=>showItems = !showItems" class="control-button">Items</button> 
+          <button @click="()=>showPosition = !showPosition" class="control-button">Positions</button> 
         </section>
-        <div class="item-list">
-          <h2>Item List</h2>
+        <div class="item-list" v-if="showItems">
+         
           <ul>
             <li @click="PlaceItem(item)" v-for="item in itemList" :key="item.Name">{{ item.Name }}</li>
+          </ul>
+          <!-- <ul>
+            <li v-for="item in itemList" :key="item.Name">
+              {{ item.Name }}
+              <button @click="placeItem(item)">Platzieren</button>
+              <button @click="removeItem(item)">Entfernen</button>
+            </li>
+          </ul> -->
+        </div>
+        <div class="position" v-if="showPosition"> 
+          <h5>Position 1</h5>
+          <h5>Position 2</h5>
+          <h5>Position 3</h5>
+          <ul>
+            <li @click="selectedPosition(position)" v-for="position in position" :key="item.Name">{{ item.Name }}</li>
           </ul>
         </div>
       </section>
@@ -32,8 +45,9 @@ import bus from '@/hooks';
 import { convertCompilerOptionsFromJson } from 'typescript';
 const socketStore = useWebSocketStore() 
 
-// const onTakeMessageRef = ref<(body: any) => void>(OnTakeMessage);
+//const onTakeMessageRef = ref<(body: any) => void>(OnTakeMessage);
 const itemList = ref({Name:"KeyCard", Description:"KeyCard, um Tür aufzuschließen"});
+ 
 
 const positions = ref([
   { id: 1, name: 'Position 1' },
@@ -42,29 +56,77 @@ const positions = ref([
 ]);
 
 const selectedPosition = ref(null);
-
+const showItems = ref(false)
+const showPosition = ref(false)
 
 onBeforeMount(() => {
-  // const GetItems = new SendEvent("GET_ITEMS") 
+  const GetItems = new SendEvent("GET_ITEMS") 
   bus.on("TAKE_MESSAGE",(body:any)=>{
     const data = <SendEvent>body
     if(data.eventName === "ON_GET_ITEMS"){
       console.log(data.data)
-      itemList.value = data.data;
+      itemList.value = data.data.Items;
       console.log(itemList.value);
 
     }
     if(data.eventName === "ON_PLACE_ITEM"){
       console.log(data.data)
-
+      handleItemPlacement(data.data);
+    }
+    if (data.eventName === "ON_REMOVE_ITEM") {
+      console.log(data.data);
+      // Logik zum Entfernen des Items einfügen
+      handleItemRemoval(data.data);
     }
   });
-  // socketStore.SendEvent(GetItems)
+  socketStore.SendEvent(GetItems)
+
+  const itemListVisible = ref(false);
+
+function toggleItemList() {
+  // Hier wird der Zustand itemListVisible umgekehrt
+  itemListVisible.value = !itemListVisible.value;
+}
+function Select(){
+  showItems.value = !showItems.value
+}
 
 }),
-function PlaceItem(item){
-
+function handleItemPlacement(item) {
+  //Logik zum Platzieren des Items implementieren
+  console.log(`Item wurde platziert: ${item.Name}`);
 }
+function handleItemRemoval(item) {
+  //Logik zum Entfernen des Items implementieren
+  console.log(`Item wurde entfernt: ${item.Name}`);
+}
+
+// function PlaceItem(item){
+//   console.log(`Item wird platziert: ${item.Name}`);
+//   socketStore.SendEvent({
+// eventName: 'PLACE_ITEM_EVENT',
+// data: item,
+// JSONString: '',
+// addData: function (key: string, value: any): void {
+// throw new Error('Function not implemented.');
+// }
+// });
+// }
+
+// function RemoveItem(item) {
+//   //Funktion zum Entfernen des Items implementieren
+//   // Diese Funktion könnte dann bei einem Klick oder anderen Benutzerinteraktion aufgerufen werden
+//   console.log(`Item wird entfernt: ${item.Name}`);
+//   socketStore.SendEvent({
+// eventName: 'REMOVE_ITEM_EVENT',
+// data: item,
+// JSONString: '',
+// addData: function (key: string, value: any): void {
+// throw new Error('Function not implemented.');
+// }
+// });
+// }
+
 function OnTakeMesage(body:any){
   
 }
